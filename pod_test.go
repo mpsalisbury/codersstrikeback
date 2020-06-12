0deea1d5cf45d5dce079991acaa141e6d5dcbd64
Mike <mike@sarge.local>
2020-06-11 23:07:49 -0700

    Better tests.

--- pod_test.go
+++ pod_test.go
@@ -1,14 +1,38 @@
 package main
 
 import (
-    "math"
-    "testing"
+	"math"
+	"testing"
 )
 
 func TestAbs(t *testing.T) {
-    got := math.Abs(-1.0)
-    want := 1.0
-    if got != want {
-        t.Errorf("Abs(-1.0) = %0.1f; want %f", got, want)
-    }
+	tests := []struct {
+    name string
+		arg, want float64
+	}{
+		{
+      name: "-1",
+			arg:  -1.0,
+			want: 1.0,
+		},
+		{
+      name: "-5",
+			arg:  -5.0,
+			want: 5.0,
+		},
+		{
+      name: "+5",
+			arg:  5.0,
+			want: 5.0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, want := math.Abs(test.arg), test.want
+			if got != want {
+				t.Errorf("Abs(-1.0) = %0.1f; want %f", got, want)
+			}
+		})
+	}
 }
